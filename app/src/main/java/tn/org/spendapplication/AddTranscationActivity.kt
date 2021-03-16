86c783ec31e505aab7b2257f15cb72f9d46376dd
Oussama Aroua <oussama.aroua@orange.com>
2021-03-16 11:47:11 +0100

    Email Validation

--- app/src/main/java/tn/org/spendapplication/AddTranscationActivity.kt
+++ app/src/main/java/tn/org/spendapplication/AddTranscationActivity.kt
@@ -3,11 +3,13 @@ package tn.org.spendapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.EditText
 import androidx.core.widget.addTextChangedListener
+import com.google.android.material.snackbar.Snackbar
 
 class AddTranscationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,12 +30,26 @@ class AddTranscationActivity : AppCompatActivity() {
 
         // Form Validation
         val validate = findViewById<Button>(R.id.validate)
+
         validate.setOnClickListener {
             val titleInput = findViewById<EditText>(R.id.title_input)
+            val emailInput = findViewById<EditText>(R.id.email_input)
             val title = titleInput.textString()
+            val email = emailInput.textString()
             if (title.isBlank()) {
                 titleInput.error = "Empty Field"
+                return@setOnClickListener
             }
+
+            // Email Validation
+            if (email.isBlank() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                emailInput.error = "Email not Valide"
+                return@setOnClickListener
+            }
+            //END Email validation
+
+            Snackbar.make(validate, "You are awesome", Snackbar.LENGTH_SHORT)
+                .show()
         }
         // END Form Validation
 
@@ -41,10 +57,11 @@ class AddTranscationActivity : AppCompatActivity() {
         val amountInput = findViewById<EditText>(R.id.amount_input)
         amountInput.addTextChangedListener {
             Log.e("TAG", "onCreate: ${it.toString()}")
-            if(it.toString().toInt() > 1000){
+            if (it.toString().toInt() > 1000) {
                 amountInput.error = "You are broke bro"
             }
         }
         //END Real time testing
+
     }
 }
