811ddb8e0559297dcd9b20823a8659987aae7fb3
Oussama Aroua <oussama.aroua@orange.com>
2021-03-16 11:05:06 +0100

    Adding AutoComplete

--- app/src/main/java/tn/org/spendapplication/AddTranscationActivity.kt
+++ app/src/main/java/tn/org/spendapplication/AddTranscationActivity.kt
@@ -2,10 +2,24 @@ package tn.org.spendapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 
 class AddTranscationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_transcation)
+
+        // Spinner / autocomplete
+        val typeList = findViewById<AutoCompleteTextView>(R.id.type_list)
+        val list = listOf("Type One", "Type Two", "Type Three")
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            list
+        )
+        typeList.setAdapter(adapter)
+        // END spinner
+
     }
 }
